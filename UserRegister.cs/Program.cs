cbce947a5e8bdbdbaa0706961cd088f4afc804e6
SumitomShome <sumitomshome@gmail.com>
2020-10-11 21:21:21 +0530

    [Sumitom] Add: Commiting UC5-Password

--- UserRegister.cs/Program.cs
+++ UserRegister.cs/Program.cs
@@ -10,10 +10,12 @@ namespace RegexUserValidation
 	    bool validLastNameResult = regex.ValidateLastName();
 	    bool validEmail = regex.ValidateEmail();
 	    bool validNumber = regex.ValidateMobileNumber();
+	    bool validPassword = regex.ValidatePassword();
             Console.WriteLine("First name: " + validResult);
 	    Console.WriteLine("Last name: " + validLastNameResult);
 	    Console.WriteLine("Email: " + validEmail);
 	    Console.WriteLine("Mobile number: " + validNumber);
+	    Console.WriteLine("Password: " + validPassword);
             Console.ReadKey();
         }
     }
--- UserRegister.cs/RegexValidation.cs
+++ UserRegister.cs/RegexValidation.cs
@@ -10,6 +10,7 @@ namespace RegexUserValidation
 	public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
 	public static string EMAIL_REGEX = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
 	public static string MOBILE_REGEX = "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$";
+	public static string PASSWORD_REGEX = "[0-9a-zA-z._+-@$]{8,}";
         public bool ValidateFirstName()
         {
             Console.WriteLine("Enter first name: ");
@@ -33,6 +34,12 @@ namespace RegexUserValidation
             Console.WriteLine("Enter country code, then give space and then provide 10 digit mobile number: ");
             string mobile = Console.ReadLine();
             return Regex.IsMatch(mobile, MOBILE_REGEX);
+        }
+	public bool ValidatePassword()
+        {
+            Console.WriteLine("Enter your password");
+            string password = Console.ReadLine();
+            return Regex.IsMatch(password, PASSWORD_REGEX);
         }
     }
 }
