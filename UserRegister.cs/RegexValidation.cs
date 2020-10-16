948f030b22a932756ec71f70022df12de55186cc
SumitomShome <sumitomshome@gmail.com>
2020-10-17 01:35:21 +0530

    [Sumitom] Refactor: Method to throw custom exceptions

--- UserRegister.cs/RegexValidation.cs
+++ UserRegister.cs/RegexValidation.cs
@@ -1,19 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 namespace RegexUserValidation
 {
     public class RegexValidation
     {
-        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
-	public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
-	public static string EMAIL_REGEX = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
-	public static string MOBILE_REGEX = "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$";
-	public static string PASSWORD_REGEX = "(?=.*?[._+-@#&*$])(?=.*[A-Z])(?=.*[0-9])([0-9a-zA-z]{7,})"
-        public bool ValidateFirstName()
+        public List <string> mail = new List<string>()
         {
-            string firstName = "Sumitom";
+            "abc@yahoo.com",
+            "abc-100@yahoo.com",
+            "abc.100@yahoo.com",
+            "abc111@abc.com",
+            "abc-100@abc.net",
+            "abc@1.com",
+            "abc@gmail.com.com",
+            "abc+100@gmail.com"
+        };
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string EMAIL_REGEX = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public static string MOBILE_REGEX = "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$";
+        public static string PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%+-])[0-9a-zA-Z][\\S]{8,}$";
+        public bool ValidateFirstName(string firstName)
+        {
             if (Regex.IsMatch(firstName, FIRST_NAME_REGEX))
             {
                 return true;
@@ -23,9 +34,8 @@ namespace RegexUserValidation
                 throw new URCustomException(URCustomException.ExceptionType.INVALID_FIRST_NAME, "Invalid first name");
             }
         }
-        public bool ValidateLastName()
+        public bool ValidateLastName(string lastName)
         {
-            string lastName = "Shome";
             if (Regex.IsMatch(lastName, LAST_NAME_REGEX))
             {
                 return true;
@@ -35,10 +45,9 @@ namespace RegexUserValidation
                 throw new URCustomException(URCustomException.ExceptionType.INVALID_LAST_NAME, "Invalid last name");
             }
         }
-        public bool ValidateEmail(string email)
+        public bool ValidateEmail(string mail)
         {
-            RegexValidation regex = new RegexValidation();
-            if (Regex.IsMatch(email, EMAIL_REGEX))
+            if (Regex.IsMatch(mail, EMAIL_REGEX))
             {
                 return true;
             }
@@ -47,9 +56,8 @@ namespace RegexUserValidation
                 throw new URCustomException(URCustomException.ExceptionType.INVALID_EMAIL, "Invalid email");
             }
         }
-        public bool ValidateMobileNumber()
+        public bool ValidateMobileNumber(string mobile)
         {
-            string mobile = "91 9433430056";
             if (Regex.IsMatch(mobile, MOBILE_REGEX))
             {
                 return true;
@@ -59,9 +67,8 @@ namespace RegexUserValidation
                 throw new URCustomException(URCustomException.ExceptionType.INVALID_MOBILE_NUMBER, "Invalid mobile number");
             }
         }
-        public bool ValidatePassword()
+        public bool ValidatePassword(string password)
         {
-            string password = "Sa@124534";
             if (Regex.IsMatch(password, PASSWORD_REGEX))
             {
                 return true;
